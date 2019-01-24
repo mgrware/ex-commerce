d6944b58104c09e0d6f27266cf989b406b91e9a4
Gilang Ramadan <mgrware@gmail.com>
2019-01-24 18:37:36 +0700

    fixing chart

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,4 +1,5 @@
 require "jwt"
+require "net/http"
 class Chart < ApplicationRecord
   has_many :items
   has_one  :billing_address
@@ -6,6 +7,10 @@ class Chart < ApplicationRecord
   def create_token(data = {})
     payload = {name: data[:name], address: data[:address], email: data[:email], phone_number: data[:phone_number], card_number: data[:card_number], code: data[:code], amount: data[:amount]}
     token = JWT.encode payload, nil, 'none'
+    url                        = "http://localhost:3000/api/v1/partner/transactions"
+    uri                        = URI.parse(url)
+    http                       = Net::HTTP.new(uri.host, uri.port)
+    response                   = http.post(uri.path, payload.to_json, {"Content-Type"  => "application/json"})
     return token
   end
 end
